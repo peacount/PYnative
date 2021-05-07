c102db48cf966e739d32483ae55c47b9e63d9b27
peacount <64988255+peacount@users.noreply.github.com>
2021-05-07 14:38:08 +0200

    Add files via upload

---
+++ Python Basic Exercises for Beginners.py
@@ -0,0 +1,17 @@
+number1 = 20
+number2 = 30
+
+print(number1 * number2)
+
+def Multiplication(num1, num2):
+    product = num1 * number2
+    if product > 1000:
+        return product
+    else: 
+        return
+    print(a*b)
+
+
+
+
+
